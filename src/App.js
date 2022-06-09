a111439ebc779e19f04fb2588343776fe59b637e
Saumya Kothuri <49059147+saum26@users.noreply.github.com>
2022-06-09 16:21:26 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Advanced from "./Components/Advanced";
+import Boost from "./Components/Boost";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Shortener from "./Components/Shortener";
+import Showcase from "./Components/Showcase";
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <Showcase/>
+      <Shortener/>
+      <Advanced/>
+      <Boost/>
+      <Footer/>
+    </>
   );
 }
-
-export default App;
